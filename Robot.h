189484da0b7d01cae1a3c8a321b03402bddf1cb2
jeannettekartchner <36744224+jeannettekartchner@users.noreply.github.com>
2023-04-29 16:46:47 -0400

    Completed parametrized constructor
    Completed parametrized constructor - check if everything is there for initializing the RobotAxis(s)
    Intended to initialize all the RobotAxis(s) of a Robot by entering the number of axis, and a table of the pins and pid values for each axis.
    Added a setHomePose - intended to put the robot arm in a home position to begin testing.
    **Check if this is what you intended for enableMotors and disableMotors

--- Robot.h
+++ Robot.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "Arduino.h"   
 #include <Bounce2.h>      // Library for debouncing inputs 
 #include "teensystep4.h"  // Library for fast, asynchronous stepper motor control on Teensy4
@@ -5,9 +6,7 @@
 #include "Joystick.h"
 using namespace TS4;      // Namespace for TeensyStep4
 
-class Robot{
-  private:
-    enum controlMode {T1,T2,AUTO,AUTOEXT};
+enum controlMode {T1,T2,AUTO,AUTOEXT};
     controlMode& operator++(controlMode& orig) {  //pre increment operator
       orig = static_cast<controlMode>(orig + 1);
       return orig;
@@ -16,6 +15,17 @@ class Robot{
     controlMode rVal = orig;
     ++orig;
     return rVal;}
+class Robot{
+  private:
+    /*enum controlMode {T1,T2,AUTO,AUTOEXT};
+    controlMode& operator++(controlMode& orig) {  //pre increment operator
+      orig = static_cast<controlMode>(orig + 1);
+      return orig;
+    }
+    controlMode operator++(controlMode& orig, int) {  //post increment operator
+    controlMode rVal = orig;
+    ++orig;
+    return rVal;}*/
 
     RobotAxis axis[5];
     int currentPose[5];
@@ -23,13 +33,16 @@ class Robot{
     bool estop;
     bool mstop;
     bool fault;
-    int faultCode
+    int faultCode;
     bool moving;
     bool calibrated;
-    Joystick pendant;
+    JoyStick pendant;
 
   public:
     Robot();
+    //pass in the number of axis and then a two dimension array of ints for pins [numAxis][7]
+    //and a two dimensional array of doubles for pid [numAxis][2]
+    Robot(int, int**, double**);
     
     void enableMotors();
     void disableMotors();
@@ -37,36 +50,51 @@ class Robot{
     void getCurrentPose();
     void getTargetPose();
     void setTargetPose();
+    void setHomePose();
     
 
 
 };//end of Robot class
-
-Robot::Robot() {
-
-      encoderPin = encPin;
-      endstopPin = endPin;
-      homingPin = homPin;
-      enablePin = enPin;
-      directionPin = dirPin;
-      stepPin = stpPin;
-      serialTxPin = txPin;
-      serialRxPin = rxPin;
-      Kp = p;
-      Ki = i;
-      Kd = d;
-      homingSpeed = 5000;
-      motor = Stepper(stpPin, dirPin);
+Robot::Robot(){}
+Robot::Robot(int numAxis, int** pins, double** pid) {
+//************* would it be easier to leave first row blank, so axis 1 is in row 1?????
+    
+  //set up the pins and pid and everything else for all the RobotAxis      
+  for(int i=0;i<numAxis;i++){
+    axis[i].setPins(pins[i][0],pins[i][1],pins[i][2], pins[i][3],pins[i][4],pins[i][5],pins[i][6],pins[i][7]);
+    axis[i].setPid(pid[i][0],pid[i][1],pid[i][2]);
+    axis[i].setHomingSpeed(5000);
+    //stepPin is [5], directionPin is [4]
+    axis[i].setMotor(pins[i][5], pins[i][4]);
      // TS4::begin(); //Begin TeensyStep4 Service
-      homeSensor = Bounce();
-      endStop = Bounce();
-      calibrated = false;
-      moving = false;
-      fault = true;
-      enabled = false;
-      faultCode = 7; //Not Calibrated
+    axis[i].setHomeSensor();
+    axis[i].setEndStop();
+    axis[i].setCalibrated(false);
+    axis[i].setMoving(false);
+    axis[i].setFault(true);
+    axis[i].setEnabled(false);
+    axis[i].setFaultCode(7); //Not Calibrated
+  }
+
       //Check EEPROM for Hard or Soft Stop Stored Positions
       //If valid, clear fault and change faultCode to 6 for "Unverified Calibration" 
 } //end of constructor
 
-void Robot::
+void Robot::enableMotors(){
+  estop = false;
+  mstop = false;
+}
+    
+void Robot::disableMotors(){
+  estop = true;
+  mstop = true;
+}
+
+void Robot::getCurrentPose(){}
+void Robot::getTargetPose(){}
+void Robot::setTargetPose(){}
+//this will place the robot 
+void Robot::setHomePose(){
+//this will set the Axis 2 in the home poisiton and Axis 3 in the home position
+}
+    
